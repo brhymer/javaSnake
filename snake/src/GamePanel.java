e44c30cadd0bd20fdcb6ed0a810e24af5efa9d11
Brhymer <leiqien28@hotmail.com>
2020-12-05 11:38:46 -0800

    snake appears on screen

--- snake/src/GamePanel.java
+++ snake/src/GamePanel.java
@@ -42,13 +42,50 @@ public class GamePanel extends JPanel implements ActionListener {
         draw(g);
     }
     public void draw(Graphics g) {
+        g.setColor(Color.green);
+        for(int i=0; i<SCREEN_HEIGHT/UNIT_SIZE; i++) {
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i =0; i<bodyParts; i++) {
+            if(i==0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(Color.gray);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
 
     }
     public void newApple() {
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
 
     }
     public void move() {
-
+        for(int i =bodyParts; i>0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch(direction) {
+        case 'U':
+            y[0] = y[0] - UNIT_SIZE;
+            break;
+        case 'D':
+            y[0] = y[0] + UNIT_SIZE;
+            break;
+        case 'L':
+            x[0] = x[0] - UNIT_SIZE;
+            break;
+        case 'R':
+            x[0] = x[0] + UNIT_SIZE;
+            break;
+        }
     }
     public void checkApple() {
 
@@ -62,7 +99,15 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
+
     }
+
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
